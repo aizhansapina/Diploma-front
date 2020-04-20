28c897ab24c5b19c55b693f1983fc0e4a42b8e79
Aizhan <aizhansapina@gmail.com>
2020-04-20 15:14:20 +0600

    added deployment changes

---
+++ build/precache-manifest.d71c5c7cc07ffa6a92203c795883a956 2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d97beaee93ed511818d9b8249f701c2",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "e911f2e0bb60b773171e",
+    "url": "/home/static/css/main.669732bd.chunk.css"
+  },
+  {
+    "revision": "9b7d3bf009df6f859e7c",
+    "url": "/home/static/js/2.f2426ad5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.f2426ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e911f2e0bb60b773171e",
+    "url": "/home/static/js/main.c367875d.chunk.js"
+  },
+  {
+    "revision": "fbeb7b7ddcea4f251e95",
+    "url": "/home/static/js/runtime-main.54cb81bf.js"
+  },
+  {
+    "revision": "8105c9ff2ca8af737c6e5d17c3b79378",
+    "url": "/home/static/media/kylie-logo-pink.8105c9ff.png"
+  },
+  {
+    "revision": "f232efa33641d37323a82e740467e6bd",
+    "url": "/home/static/media/mobile.f232efa3.jpg"
+  },
+  {
+    "revision": "83f6e1297f998b445c3bb920fed1184a",
+    "url": "/home/static/media/set-top.83f6e129.jpg"
+  },
+  {
+    "revision": "517e8681b0d6833d7c2085621c9f274f",
+    "url": "/home/static/media/sunscreen-top.517e8681.jpg"
+  },
+  {
+    "revision": "c88d9f7a64752ffde15cd5797f0c8291",
+    "url": "/home/static/media/wipes-top.c88d9f7a.jpg"
+  }
+]);
