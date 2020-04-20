2fb4e6e3d6e8f314abbe768800db2932469ae149
Aizhan <aizhansapina@gmail.com>
2020-04-20 22:43:34 +0600

    homepage deleted for derver test

---
+++ build/precache-manifest.badddbe89c089124bc7be6e7ee79b288.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aca8a8c4bf517c85f8d6909db8c72013",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1a3eabc0341d89f25ae",
+    "url": "/static/css/main.269cf6a2.chunk.css"
+  },
+  {
+    "revision": "9b7d3bf009df6f859e7c",
+    "url": "/static/js/2.f2426ad5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2426ad5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a3eabc0341d89f25ae",
+    "url": "/static/js/main.1c15f4a0.chunk.js"
+  },
+  {
+    "revision": "af7b873c7a75ce3526f1",
+    "url": "/static/js/runtime-main.36e1cc88.js"
+  },
+  {
+    "revision": "8105c9ff2ca8af737c6e5d17c3b79378",
+    "url": "/static/media/kylie-logo-pink.8105c9ff.png"
+  },
+  {
+    "revision": "f232efa33641d37323a82e740467e6bd",
+    "url": "/static/media/mobile.f232efa3.jpg"
+  },
+  {
+    "revision": "83f6e1297f998b445c3bb920fed1184a",
+    "url": "/static/media/set-top.83f6e129.jpg"
+  },
+  {
+    "revision": "517e8681b0d6833d7c2085621c9f274f",
+    "url": "/static/media/sunscreen-top.517e8681.jpg"
+  },
+  {
+    "revision": "c88d9f7a64752ffde15cd5797f0c8291",
+    "url": "/static/media/wipes-top.c88d9f7a.jpg"
+  }
+]);
