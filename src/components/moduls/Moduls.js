8e8969ea8d533260ad1f153c241c59abb0ca4738
Aizhan <aizhansapina@gmail.com>
2020-04-25 00:01:58 +0600

    added moduls api/backend connection

--- src/components/moduls/Moduls.js
+++ src/components/moduls/Moduls.js
@@ -1,16 +1,65 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import axios from "axios";
 import Header from "../shared/header/Header.js";
-
-import shop from "../../images/menu-lines.png";
-import search from "../../images/magnifying-glass.png";
-import profile_white from "../../images/account-white.png";
 
 import "../../components/shared/header/Header.scss";
 import "./Moduls.scss";
 
 class Moduls extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { moduls: [] };
+  }
+
+  componentDidMount() {
+    const token = "JWT " + sessionStorage.getItem("token");
+
+    axios
+      .get("http://104.248.114.51:8000/module_lessons/get_modules/", {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+      })
+      .then((response) => {
+        console.log("success");
+        console.log(response.data);
+        this.setState({ moduls: response.data });
+      })
+      .catch((error) => {
+        console.log("error" + error);
+      });
+  }
+
   render() {
+    console.log("state", this.state);
+    const { moduls } = this.state;
+    let modulList = moduls.map((modul) => {
+      return (
+        <div className="modul" key={modul.id}>
+          <h3 className="modul_title" key={modul.module.id}>
+            {modul.module.name}
+          </h3>
+          <div className="modul_lessons">
+            {modul.lessons.map((lessons) => (
+              <button className="lesson_button" key={lessons.id}>
+                {lessons.name}
+                <p className="lesson_button__text">
+                  {" "}
+                  Activated time: {lessons.activated_time}
+                </p>
+                <p className="lesson_button__text">
+                  {" "}
+                  Previous lessons: {modul.previous_lesson}
+                </p>
+              </button>
+            ))}
+          </div>
+        </div>
+      );
+    });
+
     return (
       <div>
         <Header />
@@ -19,57 +68,13 @@ class Moduls extends Component {
             <h1 className="student_info-fullname">
               Name Surname (Standard/ Premium/ VIP)
             </h1>
-            <h2 classname="student_info-moduls">Left: # days</h2>
-            <h2 classname="student_info-moduls">Current: Module # Lesson #</h2>
-            <h2 classname="student_info-moduls">
+            <h2 className="student_info-moduls">Left: # days</h2>
+            <h2 className="student_info-moduls">Current: Module # Lesson #</h2>
+            <h2 className="student_info-moduls">
               Next Lesson after: hours/minutes/seconds
             </h2>
           </div>
-          <div className="moduls__list">
-            <h3 className="modul_title">Modul #</h3>
-            <div className="modul_lessons">
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-            </div>
-          </div>
-          <div className="moduls__list">
-            <h3 className="modul_title">Modul #</h3>
-            <div className="modul_lessons">
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-            </div>
-          </div>
-          <div className="moduls__list">
-            <h3 className="modul_title">Modul #</h3>
-            <div className="modul_lessons">
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-              <button className="lesson_button">Lesson #</button>
-            </div>
-          </div>
+          <div className="moduls_list">{modulList}</div>
         </div>
       </div>
     );
